3314cbfa3aa078959abf098e1c11ed2dfc8683e2
Lynda Ly <lynda.ly@service.nsw.gov.au>
2025-03-05 16:21:47 +1100

    revert correct path public to staticDirs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,7 +10,7 @@ const config: StorybookConfig = {
   docs: {
     autodocs: true,
   },
-  staticDirs: ['./public', './static'],
+  staticDirs: ['../public', './static'],
 };
 
 export default config;
